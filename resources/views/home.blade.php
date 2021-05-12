d9531e72cefb638311da21475857d94b1b343555
Giuditta <giudittamarino89@gmail.com>
2021-05-12 17:38:24 +0200

    ciclati comics home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,28 @@
 
 @section('content')
 
+<div class="main-top">
+  <div class="container">
+    <div class="row">
+      @foreach ($comics as $comic)
+
+        <div class="comic col-lg-2">
+          <a href="#">
+            <div class="image" style="background-image: url('{{$comic['thumb']}}')">
+            </div>
+          </a>
+          <div class="text">
+            <a href="#">
+              <p>{{$comic['series']}}</p>
+            </a>
+          </div>
+        </div>
+
+      @endforeach
+    </div>
+  </div>
+</div>
+<div class="main-bottom">
+</div>
+
 @endsection
