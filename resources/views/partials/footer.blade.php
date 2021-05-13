2577872a092c45096a33a5c9235fd3baf1b6def3
Giuditta <giudittamarino89@gmail.com>
2021-05-13 14:32:40 +0200

    aggiunta classe active bonus

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,26 @@
+<footer>
+  <div class="footer-top">
+    <div class="col-lg-4">
+      <div class="col-lg-4"></div>
+      <div class="col-lg-4"></div>
+      <div class="col-lg-4"></div>
+    </div>
+  </div>
+  <div class="footer-bottom">
+    <div class="col lg-6">
+      <div class="col-lg-4">
+        <button type="button" name="button">sign-up now!</button>
+      </div>
+    </div>
+    <div class="col-lg-4 offset-lg-1">
+      <ul class="no-style">
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+      </ul>
+    </div>
+  </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,43 @@
+<header>
+  <div class="header-top">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 offset-lg-8">
+          <ul class="no-style">
+            <li><a href="#">dc powersm visa</a></li>
+            <li><a href="#">additional dc sites</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    </div>
+  <div class="header-bottom">
+    <div class="container">
+      <nav>
+        <div class="row">
+          <div class="navbar col-lg-12">
+            <div class="logo">
+              <img src="/images/dc-logo.png" alt="">
+            </div>
+            <ul class="no-style">
+              <li><a href="#">characters</a></li>
+              <!-- //inserisco classe con ternario se il nome della route corrente è detail -->
+              <li><a href="#" class="{{ Route::currentRouteName() == 'detail' ? 'active' : '' }}">comics</a></li>
+              <li><a href="#">movies</a></li>
+              <li><a href="#">tv</a></li>
+              <li><a href="#">games</a></li>
+              <li><a href="#">collectibles</a></li>
+              <li><a href="#">videos</a></li>
+              <li><a href="#">fans</a></li>
+              <li><a href="#">news</a></li>
+              <li><a href="#">shop</a></li>
+            </ul>
+            <input type="text" name="" value="" placeholder="Search &#128269;">
+          </div>
+        </div>
+      </nav>
+    </div>
+  </div>
+  <div class="jumbo">
+  </div>
+</header>
