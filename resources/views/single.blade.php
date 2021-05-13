6f2025f2318b4607e32b55074feeac2168156a7b
Giuditta <giudittamarino89@gmail.com>
2021-05-13 13:05:24 +0200

    inizio inserimento info comic nella pagina detail

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -1,5 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-  {{var_dump($comic)}}
+  <main id="detail">
+    <div class="image">
+      <img src="{{$comic['thumb']}}" alt="">
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8">
+          <h3>{{ $comic['title'] }}</h3>
+          <div class="row">
+            <div class="col-lg-9">
+              <p>U.S.Price: {{ $comic['price'] }}</p>
+              <p>available</p>
+            </div>
+            <div class="col-lg-3">
+              <p>Check Availability</p>
+            </div>
+            <div class="row">
+              <div class="col-lg-12">
+                <p>{{ $comic['description'] }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4"></div>
+      </div>
+    </div>
+  </main>
 @endsection
