3c0ca9cb7b233d295f9a0043a79579a5dc68d279
Giuditta <giudittamarino89@gmail.com>
2021-05-13 15:32:55 +0200

    footer-top terminato

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,10 +1,62 @@
 <footer>
   <div class="footer-top">
-    <div class="col-lg-4">
-      <div class="col-lg-4"></div>
-      <div class="col-lg-4"></div>
-      <div class="col-lg-4"></div>
+    <div class="container">
+      <div class="lists row">
+        <div class="col-lg-4">
+          <div class="row">
+            <div class="col-lg-4">
+              <h6>dc comics</h6>
+              <ul class="no-style">
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">News</a></li>
+              </ul>
+              <h6>shop</h6>
+              <ul class="no-style">
+                <li><a href="#">Shop DC</a></li>
+                <li><a href="#">Shop DC Collectibles</a></li>
+              </ul>
+            </div>
+            <div class="col-lg-4">
+              <h6>dc</h6>
+              <ul class="no-style">
+                <li><a href="#">Terms Of Use</a></li>
+                <li><a href="#">Privacy policy (New)</a></li>
+                <li><a href="#">Add Choices</a></li>
+                <li><a href="#">Advertising</a></li>
+                <li><a href="#">Jobs</a></li>
+                <li><a href="#">Subscriptions</a></li>
+                <li><a href="#">Talent Workshops</a></li>
+                <li><a href="#">CPSC Certificates</a></li>
+                <li><a href="#">Ratings</a></li>
+                <li><a href="#">Shop Help</a></li>
+                <li><a href="#">Contact Us</a></li>
+              </ul>
+            </div>
+            <div class="col-lg-4">
+              <h6>sites</h6>
+              <ul class="no-style">
+                <li><a href="#">DC</a></li>
+                <li><a href="#">MAD Magazine</a></li>
+                <li><a href="#">DC Kids</a></li>
+                <li><a href="#">DC Universe</a></li>
+                <li><a href="#">DV Power Visa</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="copyright row">
+        <div class="col-lg-6">
+          <p>All Site Content TM and &copy; 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>. All rigths reserved. <br> <a href="#">Cookies Settings</a></p>
+        </div>
+      </div>
     </div>
+    <img src="/images/dc-logo-bg.png" alt="">
   </div>
   <div class="footer-bottom">
     <div class="col lg-6">
