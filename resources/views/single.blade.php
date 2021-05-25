20a27e2233137b213b8e9f99936f383b12b799e7
Giuditta <giudittamarino89@gmail.com>
2021-05-25 19:08:39 +0200

    stile pagina detail

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -23,11 +23,11 @@
 
             <div class="green-bar row">
 
-              <div class="flex col-lg-9">
+              <div class="left flex col-lg-9">
                 <p>U.S.Price: <span id="price">{{ $comic['price'] }}</span></p>
                 <p>available</p>
               </div>
-              <div class="col-lg-3 text-center">
+              <div class="right col-lg-3 text-center">
                 <p><span id="check">Check Availability</span> <span class="caret-down">&#9660;</span></p>
               </div>
             </div>
@@ -53,29 +53,112 @@
 
     <div class="main-bottom">
       <div class="container">
-        <div class="row">
-          <div class="col-lg-6">
+        <div class="credits row">
+          <div class="talent col-lg-6">
             <h4>Talent</h4>
             <div class="art row">
-              <div class="col-lg-4">
-                <span>Art by:</span>
+              <div class="title col-lg-4">
+                <p>Art by:</p>
+              </div>
+              <div class="list col-lg-8">
                 @foreach ($comic['artists'] as $artist)
-                <span>{{ $artist }}</span>
+
+                @if (!($loop->last))
+                    <a href="#">{{$artist}}</a>,
+                  @else
+                    <a href="#">{{$artist}}</a>
+                  @endif
+
                 @endforeach
               </div>
-              <div class="col-lg-8"><span></span></div>
             </div>
             <div class="written row">
-              <div class="col-lg-4">
-                <span>Written by:</span>
+              <div class="title col-lg-4">
+                <p>Written by:</p>
+              </div>
+              <div class="list col-lg-8">
                 @foreach ($comic['writers'] as $writer)
-                <span>{{ $writer}}</span>
+
+                @if (!($loop->last))
+                    <a href="#">{{$writer}}</a>,
+                  @else
+                    <a href="#">{{$writer}}</a>
+                  @endif
+
                 @endforeach
-                </div>
-              <div class="col-lg-8"></div>
+              </div>
             </div>
           </div>
-          <div class="col-lg-6"></div>
+          <div class="specs col-lg-6">
+            <h4>Specs</h4>
+            <div class="series row">
+              <div class="title col-lg-4">
+                <p>Series:</p>
+              </div>
+              <div class="list col-lg-8">
+                <a href="#">{{ $comic['series'] }}</a>
+              </div>
+            </div>
+            <div class="price row">
+              <div class="title col-lg-4">
+                <p>U.S. Price:</p>
+              </div>
+              <div class="list col-lg-8">
+                <span>{{ $comic['price'] }}</span>
+              </div>
+            </div>
+            <div class="sale row">
+              <div class="title col-lg-4">
+                <p>On Sale Date:</p>
+              </div>
+              <div class="list col-lg-8">
+                <span>{{ $comic['sale_date'] }}</span>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div class="links row">
+          <div class="col-lg-3">
+            <div class="dc">
+              <div class="text">
+                <a href="#">digital comics</a>
+              </div>
+              <div class="image">
+                <img src="/images/buy-comics-digital-comics.png" alt="dc-icon">
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3">
+            <div class="shop">
+              <div class="text">
+                <a href="#">shop dc</a>
+              </div>
+              <div class="image">
+                <img src="/images/buy-comics-subscriptions.png" alt="shop-icon">
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3">
+            <div class="locator">
+              <div class="text">
+                <a href="#">comic shop locator</a>
+              </div>
+              <div class="image">
+                <img src="/images/buy-comics-shop-locator.png" alt="location-icon">
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3">
+            <div class="subscriptions">
+              <div class="text">
+                <a href="#">subscriptions</a>
+              </div>
+              <div class="image">
+                <img src="/images/buy-comics-merchandise.png" alt="subscriptions-icon">
+              </div>
+            </div>
+          </div>
 
         </div>
       </div>
